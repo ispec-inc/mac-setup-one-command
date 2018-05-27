6da4b927c1730696ac6bf3dd741cad6ae23db5a8
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2018-05-27 13:54:45 +0900

    Update private.zsh
    Please enter the commit message for your changes. Lines starting

--- settings/zsh/private.zsh
+++ settings/zsh/private.zsh
@@ -12,8 +12,11 @@ export PATH="$HOME/go/bin:$PATH"
 setopt print_eight_bit
 source  ~/powerlevel9k/powerlevel9k.zsh-theme
 eval "$(rbenv init - zsh)"
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(time context dir vcs)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(ip context dir vcs)
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status rbenv)
-POWERLEVEL9K_STATUS_VERBOSE=false
-POWERLEVEL9K_SHORTEN_STRATEGY="truncate_middle"
-POWERLEVEL9K_SHORTEN_DIR_LENGTH=3
+DEFAULT_USER=`whoami`
+# POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(time context dir vcs)
+# POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status rbenv)
+# POWERLEVEL9K_STATUS_VERBOSE=false
+# POWERLEVEL9K_SHORTEN_STRATEGY="truncate_middle"
+# POWERLEVEL9K_SHORTEN_DIR_LENGTH=3
